e62f8b8324f9af188acc0aa8525a897697470639
Tran Quoc Chung <bitran202@gmail.com>
2024-12-20 08:42:45 +0700

    fix env

--- admin/src/utils/axiosConfig.js
+++ admin/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:5008/api",
+    baseURL: `${process.env.REACT_APP_API_URL}/api`,
     headers: {
         'Content-Type': 'application/json'
     },
